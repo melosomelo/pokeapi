fa9403d066f74f0970cb3ce084e380fb39d85eea
melosomelo <mateusmelornrn@gmail.com>
2020-09-19 16:58:44 -0300

    Initialize project using Create React App

---
+++ README.md
@@ -0,0 +1,3 @@
+# My REACT APP 
+
+Write something here :).
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Go on :)</h1>
+    </div>
+  );
+}
+
+export default App;
