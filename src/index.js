7f56fed8cdc212919c1877bc3858dad524733cae
melosomelo <mateusmelornrn@gmail.com>
2020-09-21 11:36:53 -0300

    Remove LoadingContext component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { LoadingProvider } from "./Context/LoadingContext";
 import { AuthProvider } from "./Context/AuthContext";
 import GlobalStyle from "./styles";
 
@@ -10,10 +9,8 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
-        <LoadingProvider>
-          <GlobalStyle />
-          <App />
-        </LoadingProvider>
+        <GlobalStyle />
+        <App />
       </AuthProvider>
     </Router>
   </React.StrictMode>,
