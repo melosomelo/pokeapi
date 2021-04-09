61d765bf8c6769f90f77de8640dbdf36b730b127
Mateus <mateusmelornrn@gmail.com>
2021-04-08 23:16:29 -0300

    Feat: make build

---
+++ build/precache-manifest.242aee1c6f15d8f56d7d119a25f593c0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a27d9b0c7be91c78fa4164e2312581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "240dccfe82cc8221198c",
+    "url": "/static/css/main.6c206eaa.chunk.css"
+  },
+  {
+    "revision": "783abceec466b829bf48",
+    "url": "/static/js/2.b0ad7fff.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.b0ad7fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240dccfe82cc8221198c",
+    "url": "/static/js/main.109a88ff.chunk.js"
+  },
+  {
+    "revision": "f3578a00ee1aa37073f9",
+    "url": "/static/js/runtime-main.9623d853.js"
+  },
+  {
+    "revision": "3e1575488992f138918ae56d65607b87",
+    "url": "/static/media/close-icon.3e157548.svg"
+  },
+  {
+    "revision": "437f1ba289b0b70b65b3b91d97494afd",
+    "url": "/static/media/logo.437f1ba2.png"
+  },
+  {
+    "revision": "12e8b34c49c8b8818db5487da9a0b369",
+    "url": "/static/media/menu-icon.12e8b34c.svg"
+  },
+  {
+    "revision": "00e50e53e06e57274a84078971faf07e",
+    "url": "/static/media/pokeball.00e50e53.svg"
+  }
+]);
